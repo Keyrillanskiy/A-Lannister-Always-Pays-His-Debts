5702fc592890afd1e97736758e96d08f430ce3e8
keyrillanskiy <keyrillanskiy@gmail.com>
2020-04-06 12:17:21 +0300

    initial commit

---
+++ app/src/main/java/com/github/keyrillanskiy/city_mobil_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.keyrillanskiy.city_mobil_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
