c7ad0cc460e14f84a4fe58757455de7e230e27b7
keyrillanskiy <keyrillanskiy@gmail.com>
2020-04-06 13:13:40 +0300

    Added basic project structure and second activity

---
+++ app/src/main/java/com/github/keyrillanskiy/city_mobil_test/presentation/ui/screens/characterinfo/CharacterInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.github.keyrillanskiy.city_mobil_test.presentation.ui.screens.characterinfo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.keyrillanskiy.city_mobil_test.R
+
+class CharacterInfoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_info)
+    }
+
+}
---
+++ app/src/main/java/com/github/keyrillanskiy/city_mobil_test/presentation/ui/screens/characterlist/CharacterListActivity.kt
@@ -0,0 +1,14 @@
+package com.github.keyrillanskiy.city_mobil_test.presentation.ui.screens.characterlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.keyrillanskiy.city_mobil_test.R
+
+class CharacterListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_list)
+    }
+
+}
